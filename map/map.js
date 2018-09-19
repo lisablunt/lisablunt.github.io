66e7c22ae1dfcd9b1784cf36e9f4e5f8355c5b61
Lisa B <lisablunt1@gmail.com>
2018-09-18 20:44:59 -0400

    Update map.js

--- map/map.js
+++ map/map.js
@@ -119,7 +119,7 @@ var markers = {
       'properties': {
         'title': 'Sacramento, CA',
         'marker-symbol': 'pitch',
-        'description': '2015<br>Attended my first hackathon at Twitter HQ (SF).' 
+        'description': '2015<br>First hackathon at Twitter HQ in San Francisco.' 
       },
       'geometry': {
         'type': 'Point',
