9309b7c635ccd2746822c69d4c1366043cfd14f4
Lisa B <lisablunt1@gmail.com>
2018-09-20 15:05:51 -0400

    Caption update

--- map/map.js
+++ map/map.js
@@ -74,7 +74,7 @@ var markers = {
       'properties': {
         'title': 'Providence, RI',
         'marker-symbol': 'college',
-        'description': '2003 - 2008<br>Attended Brown University.'
+        'description': '2009<br>Graduated from Brown University.'
       },
       'geometry': {
         'type': 'Point',
